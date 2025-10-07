1c14d91a850b690f310b2d2d164a840fd21df48a
Bunny <mcflaredragon123@gmail.com>
2025-10-07 16:46:08 +0800

    2nd commit

---
+++ spyder/Integer.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  7 16:40:23 2025
+
+@author: Pei En
+"""
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
